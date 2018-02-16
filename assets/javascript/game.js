6ffb4fff5895974afa855bd66d0b6da1ed6f408e
Lauren Abney <leabney@gmail.com>
2018-02-15 23:21:15 -0600

    fixed correct word guesses

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,25 +1,54 @@
-//global variables//
+//variables//
 
 var gameWords = ["Prescott", "Witten"];
+var gameLetters = gameWords[0].toUpperCase().split('')
+var gamePlaceholders = [];
 var lettersGuessed = [];
+var lettersCorrect = [];
+
+
+//create placeholders//
+for (var i = 0; i < gameLetters.length; i++) {
+    gamePlaceholders.push("_")
+}
+
+//populate current word with placeholders//
+document.getElementById("currentWord").innerHTML = gamePlaceholders
 
 //on key press//
 document.onkeyup = function (event) {
 
     var userGuess = event.key.toUpperCase();
-     
+
     var indexed = lettersGuessed.indexOf(userGuess)
-    
-    if (indexed<0){
+    var indexedCorrect = lettersCorrect.indexOf(userGuess)
+
+    var inWord = gameLetters.indexOf(userGuess)
+    var isAlpha = function (ch) {
+        return /^[A-Z]$/i.test(ch);
+    }
+
+    if (indexed < 0 && inWord < 0 && isAlpha(userGuess) === true) {
         lettersGuessed.push(userGuess);
     }
-   
-    
-   
-    document.getElementById("currentWord").innerHTML = '__    '.repeat(gameWords[0].length)
-   
-    document.getElementById("guessedIncorrectly").innerHTML=lettersGuessed 
-        
+
+    if (indexedCorrect < 0 && inWord > -1) {
+        lettersCorrect.push(userGuess);
+    }
+
+
+    for (var i = 0; i < gamePlaceholders.length; i++) {
+
+
+        if (gameLetters[i] === userGuess) {
+            gamePlaceholders[i] = userGuess;
+            document.getElementById("currentWord").innerHTML = gamePlaceholders
+        }
+    };
+
+
+    document.getElementById("guessedIncorrectly").innerHTML = lettersGuessed
+
 
 };
 
