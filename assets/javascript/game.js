0d3597c3d266707b4e112ab1ef5f3b5842db60fa
Lauren Abney <leabney@gmail.com>
2018-02-16 09:49:49 -0600

    win counter update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -14,49 +14,57 @@ for (var i = 0; i < gameLetters.length; i++) {
     gamePlaceholders.push("_")
 }
 
-
 //populate current word with placeholders//
 document.getElementById("currentWord").innerHTML = gamePlaceholders.join("")
+
+//populate initial wins//
+document.getElementById("wins").innerHTML = wins;
 
 //populate initial guesses//
 document.getElementById("guessesRemaining").innerHTML = guessesRemaining;
 
 //on key press//
 document.onkeyup = function (event) {
-
+    //variables//
     var userGuess = event.key.toUpperCase();
-
     var indexed = lettersGuessed.indexOf(userGuess)
     var indexedCorrect = lettersCorrect.indexOf(userGuess)
-
     var inWord = gameLetters.indexOf(userGuess)
     var isAlpha = function (ch) {
         return /^[A-Z]$/i.test(ch);
     }
-
-    if (indexed < 0 && inWord < 0 && isAlpha(userGuess) === true) {
+    //push incorrect letters to incorrect div, change guesses remaining//
+    if (indexed < 0 && inWord < 0 && isAlpha(userGuess) === true && guessesRemaining>0) {
         lettersGuessed.push(userGuess);
         guessesRemaining= guessesRemaining-1;
         document.getElementById("guessesRemaining").innerHTML=guessesRemaining;
     }
 
-    if (indexedCorrect < 0 && inWord > -1) {
+
+    //push correct letters//
+    if (indexedCorrect < 0 && inWord > -1 &&guessesRemaining>0) {
         lettersCorrect.push(userGuess);
     }
 
-
+    //replace placeholders with correct letters//
     for (var i = 0; i < gamePlaceholders.length; i++) {
-
-
-        if (gameLetters[i] === userGuess) {
+        if (gameLetters[i] === userGuess && guessesRemaining>0) {
             gamePlaceholders[i] = userGuess;
             document.getElementById("currentWord").innerHTML = gamePlaceholders.join("")
         }
     };
 
-
     document.getElementById("guessedIncorrectly").innerHTML = lettersGuessed
+    
+    //win counter//
+    if(gamePlaceholders.indexOf("_")===-1){
+        wins++
+        document.getElementById("wins").innerHTML = wins
+    }
 
-
+    //stop when guessRemaining=0//
+    if (guessesRemaining===0){
+        alert("Game Over")
+    }
 };
 
