bf4257026de76d3424cf6d979d0e9dc692b907a3
Lauren Abney <leabney@gmail.com>
2018-02-16 21:38:06 -0600

    completed

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,29 @@
-var gameWords = ["Prescott", "Witten", "Touchback", "Bryant", "Bailey", "Field Goal", "Beasley", "Switzer", "Jerry's World", "Cheerleaders", "America's Team", "AT&T Stadium", "Feed Zeke", "Touchdown", "Defense"];
+var gameWords = ["Awuzie","Prescott", "Witten", "Touchback", "Bryant", "Bailey", "Field Goal", "Beasley", "Switzer", "Jerry's World", "Cheerleaders", "America's Team", "AT&T Stadium", "Feed Zeke", "Touchdown", "Defense"];
 var gameWord = gameWords[Math.floor(Math.random() * gameWords.length)];
 var wins = 0;
 var guessesRemaining = 10;
 
+function reset() {
+gameWords.splice(gameWords.indexOf(gameWord),1);
+console.log(gameWords);
+gameWord=gameWords[Math.floor(Math.random()*gameWords.length)];
+console.log(gameWord);
+guessesRemaining=10;
+lettersGuessed=[];
+document.getElementById("guessedIncorrectly").innerHTML=lettersGuessed;
+document.getElementById("guessesRemaining").innerHTML=guessesRemaining;
+gamePlaceholders=[];
+var gameLetters = gameWord.toUpperCase().split('');
+for (var i = 0; i < gameLetters.length; i++) {
+    if (gameLetters[i] === " " || gameLetters[i] === "'" || gameLetters[i] === "&") {
+        gamePlaceholders.push(gameLetters[i])
+    }
+    else {
+        gamePlaceholders.push("_")
+    }
+};
+document.getElementById("currentWord").innerHTML=gamePlaceholders.join("");
+}
 
 //variables//
 var gameLetters = gameWord.toUpperCase().split('');
@@ -30,9 +51,12 @@ document.getElementById("wins").innerHTML = wins;
 //populate initial guesses//
 document.getElementById("guessesRemaining").innerHTML = guessesRemaining;
 
+
 //on key press//
 document.onkeyup = function (event) {
+    
     //variables//
+    var gameLetters = gameWord.toUpperCase().split('');
     var userGuess = event.key.toUpperCase();
     var indexed = lettersGuessed.indexOf(userGuess);
     var indexedCorrect = lettersCorrect.indexOf(userGuess);
@@ -40,7 +64,6 @@ document.onkeyup = function (event) {
     var isAlpha = function (ch) {
         return /^[A-Z]$/i.test(ch);
     }
-
     lettersRemaining = 0;
 
     for (var k = 0; k < gamePlaceholders.length; k++) {
@@ -50,14 +73,13 @@ document.onkeyup = function (event) {
     }
 
     console.log(gameWord)
-    console.log(gamePlaceholders)
-    console.log(lettersRemaining)
-
+    console.log(gameLetters)
 
 
     //push incorrect letters to incorrect div, change guesses remaining//
     if (indexed < 0 && inWord < 0 && isAlpha(userGuess) === true && guessesRemaining > 0 && lettersRemaining > 0) {
         lettersGuessed.push(userGuess);
+        console.log(lettersGuessed);
         guessesRemaining = guessesRemaining - 1;
         document.getElementById("guessesRemaining").innerHTML = guessesRemaining;
         document.getElementById("guessedIncorrectly").innerHTML = lettersGuessed
@@ -85,8 +107,12 @@ document.onkeyup = function (event) {
     if (gamePlaceholders.indexOf("_") === -1 && lettersRemaining > 0) {
         wins++;
         document.getElementById("wins").innerHTML = wins;
-        
-  
+        reset();
+    
+    //win game//
+    if(wins===10){
+        alert("You win!");
+    }
        
 };
 };
